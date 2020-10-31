f428ed1f0681140dce2ead096b37810b3d10cd8b
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-31 19:01:57 +0530

    [Pooja]Added: Repeating till player reaches 100 position

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -9,6 +9,7 @@ public class SnakeAndLadder {
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
+    static final int WIN_POSITION = 100;
 
     private static int rollDie() {
         Random random = new Random();
@@ -17,18 +18,24 @@ public class SnakeAndLadder {
 
     private static void checkNoplayLadderSnake() {
         int playerPosition = INITIAL_POSITION;
-        Random random = new Random();
-        int option = random.nextInt(3);
-        int randomDieMove = rollDie();
-        if(option == NO_PLAY) {
-            playerPosition = playerPosition;
-            System.out.println("Player its a foult so no changes to the current position");
-        }else if(option == LADDER){
-            playerPosition += randomDieMove;
-            System.out.println("Congrats!! Player you advanced to: "+ randomDieMove +" positions");
-        }else if(option == SNAKE){
-            playerPosition -= randomDieMove;
-            System.out.println("oops!! Player you step on a snake so you will go back to "+randomDieMove+" step back");
+        while(playerPosition <= WIN_POSITION) {
+            Random random = new Random();
+            int option = random.nextInt(3);
+            int randomDieMove = rollDie();
+            if (option == NO_PLAY) {
+                playerPosition = playerPosition;
+                System.out.println("Player its a foult so no changes to the current position");
+            } else if (option == LADDER) {
+                playerPosition += randomDieMove;
+                System.out.println("Congrats!! Player you advanced to: " + randomDieMove + " positions");
+            } else if (option == SNAKE) {
+                if(playerPosition > randomDieMove) {
+                    playerPosition -= randomDieMove;
+                }else{
+                    playerPosition = 0;
+                }
+                System.out.println("oops!! Player you step on a snake so you will go back to " + randomDieMove + " step back");
+            }
         }
     }
 
