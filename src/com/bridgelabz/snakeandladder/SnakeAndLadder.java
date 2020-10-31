4281f7c0330862a90eedb62d0bd75b4b6ad64734
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-31 18:17:54 +0530

    [Pooja]Added: Initialize the variable

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,6 +3,9 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
     public static void main(String args[]) {
+        int INITIAL_POSITION = 0;
+        int NUMBER_OF_PLAYER = 1;
+
         System.out.println("Welcome to Snake and Ladder");
     }
 }
