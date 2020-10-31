a0c036c609b7b6f8ff0b7725e441d81efa0cc373
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-31 19:44:07 +0530

    [Pooja]Added:Code for Player getting exact position 100

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -9,17 +9,29 @@ public class SnakeAndLadder {
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
-    static final int WIN_POSITION = 100;
+    static final int WIN_POSITION = 4;
 
     private static int rollDie() {
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
 
-    private static void checkNoplayLadderSnake() {
+    private static int moveCondition(int playerPosition, int randomDieMove) {
+        if (playerPosition <= 0) {
+            playerPosition = INITIAL_POSITION;
+        } else if (playerPosition == WIN_POSITION) {
+            System.out.println("Player Win : "+playerPosition);
+        } else if (playerPosition >= WIN_POSITION) {
+            playerPosition = playerPosition - randomDieMove;
+        }
+        return playerPosition;
+    }
+
+    private static void repeatTillPlayerReachGoal() {
+
+        Random random = new Random();
         int playerPosition = INITIAL_POSITION;
-        while(playerPosition <= WIN_POSITION) {
-            Random random = new Random();
+        while (playerPosition <= WIN_POSITION) {
             int option = random.nextInt(3);
             int randomDieMove = rollDie();
             if (option == NO_PLAY) {
@@ -29,12 +41,16 @@ public class SnakeAndLadder {
                 playerPosition += randomDieMove;
                 System.out.println("Congrats!! Player you advanced to: " + randomDieMove + " positions");
             } else if (option == SNAKE) {
-                if(playerPosition > randomDieMove) {
+                if (playerPosition > randomDieMove) {
                     playerPosition -= randomDieMove;
-                }else{
+                } else {
                     playerPosition = 0;
                 }
                 System.out.println("oops!! Player you step on a snake so you will go back to " + randomDieMove + " step back");
+            }
+            playerPosition = moveCondition(playerPosition, randomDieMove);
+            if (playerPosition == WIN_POSITION) {
+                break;
             }
         }
     }
@@ -43,6 +59,6 @@ public class SnakeAndLadder {
 
         System.out.println("Welcome to Snake and Ladder");
         rollDie();
-        checkNoplayLadderSnake();
+        repeatTillPlayerReachGoal();
     }
 }
