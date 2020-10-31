ca97b26ca1cc1472e68944868b4ba88b101ac6dd
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-31 19:56:18 +0530

    [Pooja]Added: Code for dice position and counter after every die role

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -9,7 +9,7 @@ public class SnakeAndLadder {
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
-    static final int WIN_POSITION = 4;
+    static final int WIN_POSITION = 100;
 
     private static int rollDie() {
         Random random = new Random();
@@ -20,7 +20,7 @@ public class SnakeAndLadder {
         if (playerPosition <= 0) {
             playerPosition = INITIAL_POSITION;
         } else if (playerPosition == WIN_POSITION) {
-            System.out.println("Player Win : "+playerPosition);
+            System.out.println("Won the Game...!!! Congrats you have Reached 100th Position");
         } else if (playerPosition >= WIN_POSITION) {
             playerPosition = playerPosition - randomDieMove;
         }
@@ -31,15 +31,18 @@ public class SnakeAndLadder {
 
         Random random = new Random();
         int playerPosition = INITIAL_POSITION;
+        int rollDieCounter = 0;
         while (playerPosition <= WIN_POSITION) {
             int option = random.nextInt(3);
             int randomDieMove = rollDie();
             if (option == NO_PLAY) {
                 playerPosition = playerPosition;
                 System.out.println("Player its a foult so no changes to the current position");
+                rollDieCounter++;
             } else if (option == LADDER) {
                 playerPosition += randomDieMove;
                 System.out.println("Congrats!! Player you advanced to: " + randomDieMove + " positions");
+                rollDieCounter++;
             } else if (option == SNAKE) {
                 if (playerPosition > randomDieMove) {
                     playerPosition -= randomDieMove;
@@ -47,15 +50,17 @@ public class SnakeAndLadder {
                     playerPosition = 0;
                 }
                 System.out.println("oops!! Player you step on a snake so you will go back to " + randomDieMove + " step back");
+                rollDieCounter++;
             }
             playerPosition = moveCondition(playerPosition, randomDieMove);
             if (playerPosition == WIN_POSITION) {
                 break;
             }
         }
+        System.out.println("The total number of dice rolled is: " + rollDieCounter);
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         System.out.println("Welcome to Snake and Ladder");
         rollDie();
