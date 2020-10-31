1ff93a0bb85626708a453dbb92657b06326474b2
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-31 18:46:31 +0530

    [Pooja]Added: Checking Options for Snake, Ladder, Noplay

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,15 +4,38 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
+    static final int INITIAL_POSITION = 0;
+    static final int NUMBER_OF_PLAYER = 1;
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
+
     private static int rollDie() {
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
 
-    public static void main(String args[]) {
-        int INITIAL_POSITION = 0;
-        int NUMBER_OF_PLAYER = 1;
-        System.out.println("Welcome to Snake and Ladder");
+    private static void checkNoplayLadderSnake() {
+        int playerPosition = INITIAL_POSITION;
+        Random random = new Random();
+        int option = random.nextInt(3);
         int randomDieMove = rollDie();
+        if(option == NO_PLAY) {
+            playerPosition = playerPosition;
+            System.out.println("Player its a foult so no changes to the current position");
+        }else if(option == LADDER){
+            playerPosition += randomDieMove;
+            System.out.println("Congrats!! Player you advanced to: "+ randomDieMove +" positions");
+        }else if(option == SNAKE){
+            playerPosition -= randomDieMove;
+            System.out.println("oops!! Player you step on a snake so you will go back to "+randomDieMove+" step back");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to Snake and Ladder");
+        rollDie();
+        checkNoplayLadderSnake();
     }
 }
