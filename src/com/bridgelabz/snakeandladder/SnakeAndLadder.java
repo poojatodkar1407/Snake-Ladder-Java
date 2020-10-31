13b538a851e8f460567175b7d289a9ee2c668a8e
poojatodkar1407 <poojatodkar124@gmail.com>
2020-10-31 18:25:25 +0530

    [Pooja]Added: function for roll die

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
+
+    private static int rollDie() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
 
     public static void main(String args[]) {
         int INITIAL_POSITION = 0;
         int NUMBER_OF_PLAYER = 1;
-
         System.out.println("Welcome to Snake and Ladder");
+        int randomDieMove = rollDie();
     }
 }
